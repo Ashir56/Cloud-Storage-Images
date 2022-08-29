406f4bf8f17c1970784777bf01cf44143c1288ef
sai.web2 <ashir.rasheedx@gmail.com>
2022-08-29 16:09:23 +0500

    removed downloading of blob as string

--- cloudsql.py
+++ cloudsql.py
@@ -5,12 +5,6 @@ import io
 import sys
 import os
 
-
-def write_file(data, filename):
-    # Convert binary data to proper format and write it on Hard Disk
-    with open(filename, 'w') as file:
-        file.write(data)
-
 # conn = psycopg2.connect(
 #     database="kona-production", user="postgres",
 #     password="incloud@2019", host="35.188.112.148")
@@ -23,22 +17,25 @@ client = storage.Client()
 # bucket = client.get_bucket('kona-2c5bc.appspot.com')
 bucket = client.get_bucket('kona-staging.appspot.com')
 db = conn.cursor()
-db.execute("SELECT avatar from users WHERE avatar IS NOT NULL and avatar <> '';")
-records = db.fetchall()
 
+
+db.execute("SELECT id, avatar from users WHERE avatar IS NOT NULL and avatar <> '';")
+records = db.fetchall()
+types = ['.jpg', '.jpeg', '.png', '.PNG', '.JPEG', '.JPG']
 i = 0
 sizes = []
 for record in records:
     if record is not None:
-        print(record)
+        # print(record)
         i += 1
-        image = record[0]
-        # print(image)
-        if ('.jpg' in image or '.png' in image) or ('.JPEG' in image or '.PNG' in image):
+        image = record[1]
+        print(image)
+        res = any(map(image.__contains__, types))
+        if res:
             blob = bucket.get_blob(image)
-            content = blob.download_as_string()
-            sizes.append((content.__sizeof__() /1024)/1024)
-            # print(sizes)
+            size = (blob.size/1024)/1024
+            sizes.append(size)
+            print(format(size, ".2f"), "MB")
         # bytes = io.BytesIO(content)
         # im = Image.open(bytes)
         #
